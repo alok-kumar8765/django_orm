74bba11a938141dac970c04823ef3428b4680c9a
alok kumar <85283226+alok-techqware@users.noreply.github.com>
2022-06-07 16:01:25 +0530

    push

--- Django_ORM/django_orm/urls.py
+++ Django_ORM/django_orm/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('student/',include('app.urls',namespace='student')),
+    
     # path('employee/',include('employee.urls')),
 ]
